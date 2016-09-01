5628fcb261a1c01e86df79afff5a4e61d90cc757
Egor Kislitsyn <egor@kislitsyn.com>
2016-09-01 14:06:30 +0700

    adds coveralls

--- mix.exs
+++ mix.exs
@@ -8,7 +8,9 @@ defmodule Paasaa.Mixfile do
       elixir: "~> 1.3",
       description: description(),
       package: package(),
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: ["coveralls": :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test]
     ]
   end
 
@@ -22,7 +24,8 @@ defmodule Paasaa.Mixfile do
       {:ex_doc, "~> 0.12", only: :dev},
       {:mix_test_watch, "~> 0.2", only: :dev},
       {:benchfella, "~> 0.3.0", only: :dev},
-      {:credo, "~> 0.4", only: [:dev, :test]}
+      {:credo, "~> 0.4", only: [:dev, :test]},
+      {:excoveralls, "~> 0.5", only: :test}
     ]
   end
 
