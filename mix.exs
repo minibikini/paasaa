aff023e0d53ac3565f8a87f7bdbace37c3e07736
Egor Kislitsyn <egor@kislitsyn.com>
2025-09-05 22:04:59 +0400

    Add test support directory to elixirc paths

--- mix.exs
+++ mix.exs
@@ -6,6 +6,7 @@ defmodule Paasaa.Mixfile do
       app: :paasaa,
       version: "0.6.0",
       elixir: "~> 1.18",
+      elixirc_paths: elixirc_paths(Mix.env()),
       description: description(),
       package: package(),
       deps: deps(),
@@ -31,6 +32,9 @@ defmodule Paasaa.Mixfile do
   def application do
     [extra_applications: [:logger]]
   end
+
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
 
   defp deps do
     [
