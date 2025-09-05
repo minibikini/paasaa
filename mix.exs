0830861499148e3feac6ce778761aa50e360fd52
Egor Kislitsyn <egor@kislitsyn.com>
2025-09-06 00:06:13 +0400

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -43,7 +43,7 @@ defmodule Paasaa.Mixfile do
       {:credo, "~> 1.7.0", only: [:dev, :test]},
       {:excoveralls, "~> 0.18.3", only: :test},
       {:jsx, "~> 3.1.0", only: :dev},
-      {:req, "~> 0.5.15"},
+      {:req, "~> 0.5.15", only: :dev},
       {:dialyxir, "~> 1.4.3", only: [:dev]}
     ]
   end
